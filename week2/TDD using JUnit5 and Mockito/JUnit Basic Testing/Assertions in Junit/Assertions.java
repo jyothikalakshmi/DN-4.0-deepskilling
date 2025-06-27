dadd7d2266b4a1e41c66a4eab3b086e64a7885bd
Jyothika Lakshmi M <119155001+jyothikalakshmi@users.noreply.github.com>
2025-06-27 19:01:46 +0530

    Created Assertions.java

---
+++ week2/TDD using JUnit5 and Mockito/JUnit Basic Testing/Assertions in Junit/Assertions.java
@@ -0,0 +1,15 @@
+public class Assertions{
+    public int add(int a, int b){
+        return a+b;
+    }
+    public boolean Isgreater(int a, int b){
+        return a>b;
+    }
+    public Object returnNull() {
+        return null;
+    }
+
+    public Object returnNotNull() {
+        return new Object();
+    }
+}
