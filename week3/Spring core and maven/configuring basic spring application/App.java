a81388b12609991bcd6a0748c65a066d5d1e8bad
Jyothika Lakshmi M <119155001+jyothikalakshmi@users.noreply.github.com>
2025-07-03 22:34:46 +0530

    Updated App.java

--- week3/Spring core and maven/configuring basic spring application/App.java
+++ week3/Spring core and maven/configuring basic spring application/App.java
@@ -6,6 +6,8 @@ public class App {
     public static void main(String args[]){
         ApplicationContext context= new ClassPathXmlApplicationContext("applicationContext.xml");
         BookService bs= (BookService) context.getBean("bookService");
+        BookRepository br=(BookRepository) context.getBean("bookRepository");
+        br.print();
         bs.greet()
     }
 }
