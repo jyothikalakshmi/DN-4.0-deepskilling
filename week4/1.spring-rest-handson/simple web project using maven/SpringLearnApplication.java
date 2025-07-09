c87f9a5d20dfad44c0e105fce570b993c600caf7
Jyothika Lakshmi M <119155001+jyothikalakshmi@users.noreply.github.com>
2025-07-10 01:11:59 +0530

    Updated SpringLearnApplication.java

--- week4/1.spring-rest-handson/simple web project using maven/SpringLearnApplication.java
+++ week4/1.spring-rest-handson/simple web project using maven/SpringLearnApplication.java
@@ -1,15 +1,16 @@
 package com.cognizant.spring_learn;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class SpringLearnApplication {
-
+  private static final Logger logger=LoggerFactory.getLogger(SpringLearnApplication.class);
 	public static void main(String[] args) {
-		System.out.println("starting Spring learn app");
+		logger.info("starting Spring learn app");
 		SpringApplication.run(SpringLearnApplication.class, args);
-		System.out.println("spring learn app started");
+		logger.info("spring learn app started");
 	}
 
 }
