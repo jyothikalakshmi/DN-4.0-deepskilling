267e832efc39638a91e2a2f96201a7db194c606c
Jyothika Lakshmi M <119155001+jyothikalakshmi@users.noreply.github.com>
2025-07-10 17:06:07 +0530

    Created SpringLearnApplication.java

---
+++ week4/2.spring-rest-handson/REST-Get Country based on country code/SpringLearnApplication.java
@@ -0,0 +1,16 @@
+package com.cognizant.spring_learn;
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringLearnApplication.class, args);
+	}
+}
