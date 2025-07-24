0573c76840d7fb821e603f0096aca0138e11ce49
Jyothika Lakshmi M <119155001+jyothikalakshmi@users.noreply.github.com>
2025-07-24 12:04:57 +0530

    Created Contact.js

---
+++ week6/React/2. ReactJS- HOL/Contact.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+class Contact extends Component{
+render() {
+  return (
+    <div>
+     <h3> Welcome to the contact page of Student ManagementPortal</h3>
+    </div>
+  )
+}
+}
+
+export default Contact;
