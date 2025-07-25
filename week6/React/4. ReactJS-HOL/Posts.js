77b6ba4a80b166ba38fa676c9d5a67feba3485e0
Jyothika Lakshmi M <119155001+jyothikalakshmi@users.noreply.github.com>
2025-07-25 17:13:10 +0530

    Created Posts.js

---
+++ week6/React/4. ReactJS-HOL/Posts.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import Post from "./Post";
+
+class Posts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      hasError: false,
+    };
+  }
+
+  loadPosts = () => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => {
+        const postsList = data.map(
+          (post) => new Post(post.id, post.title, post.body)
+        );
+        this.setState({ posts: postsList });
+      })
+      .catch((error) => {
+        console.error("Error fetching posts:", error);
+        this.setState({ hasError: true });
+      });
+  };
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  componentDidCatch(error, info) {
+    alert("An error occurred in the Posts component!");
+    console.error("Error:", error, "Info:", info);
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h2>Something went wrong.</h2>;
+    }
+
+    return (
+      <div>
+        <h1>Posts</h1>
+        {this.state.posts.map((post) => (
+          <div key={post.id}>
+            <h2>{post.id}</h2>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <hr />
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Posts;
